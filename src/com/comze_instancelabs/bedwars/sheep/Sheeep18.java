a1c66f83a09577e811cad95625a7a0c81d834e8a
InstanceLabs <danielratke97@hotmail.de>
2014-12-30 19:06:27 +0100

    Fixes #2

--- src/com/comze_instancelabs/bedwars/sheep/Sheeep18.java
+++ src/com/comze_instancelabs/bedwars/sheep/Sheeep18.java
@@ -15,19 +15,19 @@ import net.minecraft.server.v1_8_R1.World;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Sheep;
+import org.bukkit.event.entity.EntityTargetEvent;
 
 public class Sheeep18 extends EntitySheep implements Sheeep {
 
 	public Sheeep18(World world, Entity target) {
 		super(world);
+		this.locX = target.locX;
+		this.locY = target.locY;
+		this.locZ = target.locZ;
 		try {
 			Field b = this.goalSelector.getClass().getDeclaredField("b");
 			b.setAccessible(true);
 			b.set(this.goalSelector, new ArrayList());
-			Field field = Navigation.class.getDeclaredField("e");
-			field.setAccessible(true);
-			AttributeInstance ai = (AttributeInstance) field.get(this.getNavigation());
-			ai.setValue(128);
 			this.getAttributeInstance(GenericAttributes.b).setValue(128D);
 			this.getAttributeInstance(GenericAttributes.d).setValue(0.37D);
 		} catch (Exception e) {
@@ -35,7 +35,7 @@ public class Sheeep18 extends EntitySheep implements Sheeep {
 		}
 		this.goalSelector.a(0, new PathfinderGoalMeleeAttack18(this, EntityHuman.class, 1D, false));
 		// this.goalSelector.a(0, new PathfinderGoalFollowParent(this, 1.1D));
-		this.setGoalTarget((EntityLiving) target);
+		this.setGoalTarget((EntityLiving) target, EntityTargetEvent.TargetReason.OWNER_ATTACKED_TARGET, false);
 		((Sheep) this.getBukkitEntity()).setTarget((LivingEntity) target.getBukkitEntity());
 	}
 	
